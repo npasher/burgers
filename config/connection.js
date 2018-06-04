07b55b88ac8e78de00573970d4f3f83608348f92
npasher <n.p.asher@gmail.com>
2018-06-03 22:35:10 -0400

    heroku adjustment

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,12 @@ if (process.env.JAWSDB_URL){
     connection=mysql.createConnection(process.env.JAWSDB_URL);
 }else{
     connection=mysql.createConnection({
-        port:3306,
         host:"localhost",
         user:"root",
         password:"",
         database:"burgers_db"
     });
-}
+};
 //Connecting to MySQL.//
 connection.connect(function(err){
     if (err){
